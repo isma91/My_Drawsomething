cfae77dbb6acf4fd3f84b73910d8749c0620fdb3
aydogm_i <ismaydogmus@gmail.com>
2015-09-07 16:56:48 +0200

    The redirection page (FRONT)

---
+++ index.php
@@ -0,0 +1,16 @@
+<?php
+/**
+* Index.php
+*
+* Fichier pour rediriger vers la page d'index
+* 
+* PHP Version 5.5.12
+*
+* @category Reidrection
+* @package  Vue
+* @author   aydogm_i <ismail.aydogmus@epitech.eu>
+* @license  http://opensource.org/licenses/gpl-license.php GNU Public License
+* @link     index.php
+*/
+header("Location: vue/");
+?>
